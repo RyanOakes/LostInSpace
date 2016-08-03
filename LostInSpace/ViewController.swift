b38cf29e9d613aa5d9d3438a3f8d70940f0dc62d
oakes <oakesonline@gmail.com>
2016-08-03 17:16:16 -0400

    Wiring up name elements for user interaction with story

--- LostInSpace/ViewController.swift
+++ LostInSpace/ViewController.swift
@@ -10,10 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    enum Error: ErrorType {
+        case NoName
+    }
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -22,11 +29,29 @@ class ViewController: UIViewController {
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if segue.identifier == "startAdventure" {
-            if let pageController = segue.destinationViewController as? PageController {
-                pageController.page = Adventure.story
+            do {
+                if let name = nameTextField.text {
+                    if name == "" {
+                        throw Error.NoName
+                    }
+                    if let pageController = segue.destinationViewController as? PageController {
+                        pageController.page = Adventure.story(name)
+                    }
+                    
+                }
+    
+                } catch Error.NoName {
+                    let alertController = UIAlertController(title: "Name Not Provided", message: "Please provide a name!", preferredStyle: .Alert)
+                    let action = UIAlertAction(title: "OK", style: .Default, handler: nil)
+                    alertController.addAction(action)
+                    
+                    presentViewController(alertController, animated: true, completion: nil)
+                } catch let error {
+                    fatalError("\(error)")
+                }
+                    
             }
         }
     }
-    
-    
-}
+
+
